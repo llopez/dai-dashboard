f3be66f9c184b6bf87592e0d428967eddf87881f
Luis Lopez <luis.lopez@altoros.com>
2022-11-23 11:53:05 -0300

    recharts example

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import { Container } from "react-bootstrap"
+import { Dashboard } from "../components/Dashboard"
 
 const Home = () => {
   return (
     <Container>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius numquam unde mollitia nam ducimus similique, dolores dolor nisi exercitationem deserunt, libero suscipit sed consectetur aliquid. Nulla numquam repudiandae unde veritatis.</p>
+      <Dashboard />
     </Container>
   )
 }
